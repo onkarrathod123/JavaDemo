5d5103910dc2cb7e579d1041b75e07975660e822
onkarrathod123 <onkar.rathod18@gmail.com>
2024-10-02 13:20:56 +0530

    This is my second commit

--- src/com/practice/HelloWord.java
+++ src/com/practice/HelloWord.java
@@ -5,6 +5,7 @@ import java.security.DomainCombiner;
 public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello madarchod");
 	}
 
 }
